8e49759be54cd400c91d331219d3791bbdc9e8d7
Gustavo Aquino da Silva <gustavo.silva.ps@sequoialog.com.br>
2022-12-25 19:15:18 -0300

    Arquivos do projeto.

---
+++ src/components/lista/index.tsx
@@ -0,0 +1,28 @@
+import { ITarefa } from '../../types/Tarefa';
+import Item from './item';
+import style from './lista.module.scss';
+
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+
+            <ul>
+                {tarefas.map(item => (
+                    <Item 
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
+                        {...item}
+                    />
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
